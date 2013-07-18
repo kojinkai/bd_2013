e94d3ced157765f0219026551a20e7631ea8f4f7
Lewis Nixon <lewcfn@gmail.com>
2013-07-19 00:44:06 +0100

    tidying up the main js file

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,17 @@
 	$('.waypoint').backfill({
 		offset: $( '.navbar').height()
 	});
+  // add class to fixed navbar
+  $('.navbar .navigation').scrollover();
+
+  // stretch each panel to full screen
+  $('.waypoint').backfill({
+    offset: $( '.navbar').height()
+  });
+  var $paneTarget = $('#page-wrap');
+  $('a[href="#intro"]').click(function(){
+    console.log('shitknacker: ', $paneTarget);
+    var target = $paneTarget.find('#design');
+    $paneTarget.stop().scrollTo( target );
+  });
 })(jQuery);
