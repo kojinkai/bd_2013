58169eeddf04514a326092a265d5daae66b9ff32
Lewis Nixon <lewcfn@gmail.com>
2013-07-19 23:48:50 +0100

    adding the new footer and introducing a contact template.

--- assets/js/bd_2013.js
+++ assets/js/bd_2013.js
@@ -482,19 +482,32 @@
 
 })(jQuery);// Our main JS file
 (function($) {
+    
     var $navbar = $('.navbar'),
-        navOffset = $navbar.height();
+        navOffset = $navbar.height(),
+        
+        $waypoints = $('.waypoint'),
+        $lastWaypoint = $('[data-scroll-target]').last();
 
+    // The menu scroll plugin 
     $('.navigation').scrollover();
+
+    // The backstretch plugin
     $('.waypoint').backfill({
         offset: navOffset
     });
 
     $(document).on('click.scrollTo.data-api', '[data-scroll-target]', function (e) {
-        var $t = $(this);
-        $.scrollTo($t.dataAttr('scroll-target'), {
-            offset: { top: -navOffset, left: 0 },
-            duration: 800
+        
+        e.preventDefault();
+
+        var t = this,
+            target = $(t).dataAttr('scroll-target');
+
+        $.scrollTo(target, {
+            
+          offset: { top: -navOffset, left: 0 },
+          duration: 800
         });
     });
 })(jQuery);
