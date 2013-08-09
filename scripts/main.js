aa5e31c1420e1bbbbffcc3b0c5e30a8c35e79929
Lewis Nixon <lewcfn@gmail.com>
2013-08-09 15:09:33 +0100

    removing uneeded doc.ready block

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ yepnope({
     'jQ_asyncPlugins': 'assets/js/desktop.min.js'
   },
   callback: function (url, result, key) {
+    
     console.log('url iZZZ: ', url, "\nresult iz: ", result, "\nkey iz: ", key);
 
     if (result) {
@@ -17,13 +18,5 @@ yepnope({
       BD.enhance(BD.unstage());
     }
 
-    // Our main JS file
-    $(document).ready(function() {
-      // doc ready here because grunt concat is not managing
-      // dependencies so calls to non-existent functions are
-      // breaking the shit. Get require.js going at some point
-      // BD.init();
-
-    });    
   }
 });
