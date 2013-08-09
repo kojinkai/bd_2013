2c18ecc0d82d58d62caf05f042907af05f934def
Lewis Nixon <lewcfn@gmail.com>
2013-08-09 18:03:53 +0100

    updating the Main js file with the complete callback

--- assets/js/bd_2013.js
+++ assets/js/bd_2013.js
@@ -478,30 +478,16 @@
 
 BD.init();yepnope({
   test: BD.isMobile(),
-
   // is this desktop?
   nope: {
     'jQ_asyncPlugins': 'assets/js/desktop.min.js'
   },
   callback: function (url, result, key) {
-    console.log('url iZZZ: ', url, "\nresult iz: ", result, "\nkey iz: ", key);
+    
+    BD.enhance(BD.unstage());
 
-    if (result) {
-      console.log("foo");
-      BD.unstage();
-    }
-
-    else {
-      BD.enhance(BD.unstage());
-    }
-
-    // Our main JS file
-    $(document).ready(function() {
-      // doc ready here because grunt concat is not managing
-      // dependencies so calls to non-existent functions are
-      // breaking the shit. Get require.js going at some point
-      // BD.init();
-
-    });    
+  },
+  complete: function() {
+    BD.unstage();
   }
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,15 @@
 yepnope({
   test: BD.isMobile(),
-
   // is this desktop?
   nope: {
     'jQ_asyncPlugins': 'assets/js/desktop.min.js'
   },
   callback: function (url, result, key) {
     
-    console.log('url iZZZ: ', url, "\nresult iz: ", result, "\nkey iz: ", key);
+    BD.enhance(BD.unstage());
 
-    if (result) {
-      console.log("foo");
-      BD.unstage();
-    }
-
-    else {
-      BD.enhance(BD.unstage());
-    }
-
+  },
+  complete: function() {
+    BD.unstage();
   }
 });
